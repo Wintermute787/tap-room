8780f5099a3fd26f4844a37b222fea396d6c918b
Brooke Kullberg and Joe Barnes <joe@>
2019-06-12 11:55:29 -0700

    add forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Keg} from './models/kegs.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tap Room';
+
+  thisKeg: Keg = new Keg('Pepsi', 'PepsiCO', 2, 'Cola', 124)
+
+  thisKeg2: Keg = new Keg('Dr.Pepper', 'PepsiCO', 1, 'Pepper', 124)
+
+  thisKeg3: Keg = new Keg('Sprite', 'CocaCola', 5, 'LemonLime', 124)
+
+  thisKeg4: Keg = new Keg('Squirt', 'CocaCola', 3, 'Grapefruit', 60)
+
+  thisKeg5: Keg = new Keg('Coke', 'CocaCola', 3, 'Cola', 40)
+
+  kegs: Keg[] = [
+    this.thisKeg, 
+    this.thisKeg2,
+    this.thisKeg3,
+    this.thisKeg4,
+    this.thisKeg5
+  ]
+  
+  new: boolean = false;
+  list: boolean = true;
+  customer: boolean = false; 
+
+  open(id) {
+    if (id === 1) {
+      this.customer = false;
+      this.list = false;
+      this.new = true;
+    } else if (id === 2) {
+      this.new = false;
+      this.customer = false;
+      this.list = true;
+    } else {
+      this.new = false;
+      this.list = false;
+      this.customer = true;
+    }
+  }
+
+  addNewKeg(name: string, brand: string, price: number, flavor: string) {
+    let newKeg = new Keg(name, brand, price, flavor, 134);
+    this.kegs.push(newKeg);
+    // sessionStorage.setItem('keg', JSON.stringify(newKeg));
+    // console.log(sessionStorage.getItem('keg'));
+    
+    
+    console.log(newKeg);
+
+  }
+
 }
+
+
+// sessionStorage.clear()
---
+++ src/app/models/kegs.model.ts
@@ -0,0 +1,9 @@
+import { AppComponent } from "../app.component";
+
+export class Keg {
+    constructor(public name: string, public brand: string, public price: number, public flavor: string, public contents: number) {}
+
+    pour(){
+        return this.contents--;
+    }
+}
